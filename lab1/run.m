86756fc44bc9be5433fd1564423afa14d4ca5095
mailtjerk <mailtjerk@87a579e4-a2c4-11de-a590-a9c80ee327d0>
2009-09-16 16:06:34 +0000

    bijna af

--- lab1/run.m
+++ lab1/run.m
@@ -1,21 +1,54 @@
 close all
-i=im2double(rgb2gray(imread('images/nemo1.jpg')));
-h = figure
-imshow(i);
-% nbins = 10;
-% figure;
-% hist(i, nbins);
-rect = getrect(h)
+img=im2double(rgb2gray(imread('images/nemo1.jpg')));
+h = figure;
+imshow(img);
 
+rect = getrect(h)
 x = rect(1);
 y = rect(2);
 w = rect(3);
 h = rect(4);
 
-nemo = i(y:y+h, x:x+w);
+        
+%x = 249;
+%y = 104;
+%w = 62;
+%h = 71;
+
+nemo = img(y:y+h, x:x+w);
 figure
 imshow(nemo)
 
+nbins = 50;
 
+histogram = hist(nemo,[0:1/nbins:1]);
+histogram = sum(histogram');
+histogramNorm = histogram / sum(sum(histogram))
+
+
+[imgH, imgW] = size(img)
+imgProb = zeros(imgH, imgW);
+
+% img = nemo;
+% [imgH, imgW] = size(img)
+
+for i = 1:imgH
+    for j = 1:imgW
+        i
+        p = img(i,j);
+        %pHist = hist(p, nbins);
+
+        if p == 0
+            pHist = 1;
+        else
+            pHist = ceil(p*nbins);
+        end
+
+        imgProb(i,j) = histogramNorm(pHist);
+    end
+end
+
+figure;
+imshow(imgProb, []);
 
 
