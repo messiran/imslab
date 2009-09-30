41fbe5c52919314f0f6e91cf9265d3b78c0e4b02
bjbuter <bjbuter@gmail.com>
2009-09-30 10:16:18 +0000

    added color transform functions

--- lab1/img2histloc2D.m
+++ lab1/img2histloc2D.m
@@ -1,13 +1,12 @@
-% nbins 3d [r, g, b] 
-% img = (MxN) xd
+% nbins 2d [x, y] 
+% img = (MxN) x2
 % gives all images in a picture a number corresponding to a 3d histogram
 % bin.
-function R = img2histloc(img, nbins)
+function R = img2histloc2D(img, nbins)
 
 %imgSerialized = [img(:,1), img(:,:,2), img(:,:,3)]
 x = floor( img(:,1)*(nbins(1)-1) ) + 1;
 y = floor( img(:,2)*(nbins(2)-1) ) + 1;
-z = floor( img(:,3)*(nbins(3)-1) ) + 1;
 
-R = x + (y-1)*nbins(1) + (z-1)*nbins(1)*nbins(2);
+R = x + (y-1)*nbins(1);
 
--- lab1/run.m
+++ lab1/run.m
@@ -4,40 +4,26 @@ clear all
 img1=im2double(imread('images/nemo1.jpg'));
 img2=im2double(imread('images/nemo2.jpg'));
 
-C1 = makecform('srgb2xyz');
-C2 = makecform('xyz2xyl');
-C3 = makecform('xyl2xyz');
-C4 = makecform('xyz2srgb');
+% goto xy space
+I_xyl1 = rgb2xy(img1);
+I_xyl2 = rgb2xy(img2);
 
-I_xyz1 = applycform(img1,C1);
-I_xyl1 = applycform(I_xyz1,C2);
-[M, N, P] = size(I_xyl1);
-I_xyl1(:,:,3) = ones(M, N);
-
-I_xyz2 = applycform(img2,C1);
-I_xyl2 = applycform(I_xyz2,C2);
-[M, N, P] = size(I_xyl2);
-I_xyl2(:,:,3) = ones(M, N);
-
-I_xyz1 = applycform(I_xyl1,C3);
-I_rgb1 = applycform(I_xyz1,C4);
- 
-%imshow(I_rgb1)
-
+%show original
 h = figure;
 subplot(2,2,1);
 imshow(img1);
 img = I_xyl1;
 
-
+%get region
 rect = getrect(h)
-x = rect(1);
-y = rect(2);
-w = rect(3);
-h = rect(4);
+x = floor(rect(1));
+y = floor(rect(2));
+w = floor(rect(3));
+h = floor(rect(4));
 
+% show xy image converted back
 subplot(2,2,2);
-imshow(img);
+imshow(xy2rgb(img));
        
 % x = 249;
 % y = 104;
@@ -51,9 +37,11 @@ nemo = img(y:y+h, x:x+w, :);
 [M, N, P] = size(nemo);
 nemoReshaped = reshape(nemo, [M*N, P]);
 
-NBins = [20, 20, 1];
-locs = img2histloc(nemoReshaped, NBins);
+NBins = [20, 20];
 
+% get locks
+locs = img2histloc2D(nemoReshaped, NBins);
+% get counts
 counts = full(sparse(locs, ones(M*N,1), ones(M*N,1), prod(NBins), 1));
 p = counts/(M*N);
 
@@ -64,14 +52,15 @@ img = I_xyl2;
 [M, N, P] = size(img);
 imgReshaped = reshape(img, [M*N, P]);
 
-imgloc = img2histloc(imgReshaped, NBins);
+imgloc = img2histloc2D(imgReshaped, NBins);
 result = p(imgloc);
 result = reshape(result, [M,N]);
 
 subplot(2,2,3)
 imshow(img2)
 subplot(2,2,4)
-imshow(img)
+imgT = xy2rgb(img2)
+imshow(imgT)
 figure;
 imshow(result,[]);
 
