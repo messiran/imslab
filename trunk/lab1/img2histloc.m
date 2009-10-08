04aa4adf70888e6c73eb4df79c68168273578e87
bjbuter@gmail.com <bjbuter@gmail.com@87a579e4-a2c4-11de-a590-a9c80ee327d0>
2009-10-08 16:00:51 +0000

    ik hoop dat allaes zo werkt, added a lot, works with hiostogramdistances, and added settings at the start

--- trunk/lab1/img2histloc.m
+++ trunk/lab1/img2histloc.m
@@ -2,12 +2,37 @@
 % img = (MxN) xd
 % gives all images in a picture a number corresponding to a 3d histogram
 % bin.
-function R = img2histloc(img, nbins)
+function histloc = img2histloc(img, nbins)
+%IMG2HISTLOC(img, nbins) Transform an image into an image with at each
+%pixel location a direct index into a histogram with nbins.
+% 
+% histloc = IMG2HISTLOC(img, nbins)
+% img, an image with 1,2 or 3 color dimensions
+% nbins, a vector specifying the number of bins in each color dimension
+%        it should have the same dimensions as the image has color dimensions
 
-%imgSerialized = [img(:,1), img(:,:,2), img(:,:,3)]
+
+%init variables
+x=0; y=0; z=0;
+[M N P] = size(img);
+
+% make nbins same dimension as colorspace in image
+%if length(nbins) == 1
+%    nbins = nbins * ones(1, P);
+%end
+
+% always calculate x
 x = floor( img(:,1)*(nbins(1)-1) ) + 1;
-y = floor( img(:,2)*(nbins(2)-1) ) + 1;
-z = floor( img(:,3)*(nbins(3)-1) ) + 1;
 
-R = x + (y-1)*nbins(1) + (z-1)*nbins(1)*nbins(2);
+% only y if dimensions >= 2
+if P >= 2
+    y = (floor( img(:,2)*(nbins(2)-1) ))*nbins(1);
+end
+
+% only y if dimensions == 3
+
+if P == 3
+    z = (floor( img(:,3)*(nbins(3)-1) ))*nbins(1)*nbins(2);
+end
+histloc = x + y + z;
 
