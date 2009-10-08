6aae72ab7ab378cee08f849927bb38f3617fe42a
bjbuter <bjbuter@gmail.com>
2009-10-08 16:00:51 +0000

    ik hoop dat allaes zo werkt, added a lot, works with hiostogramdistances, and added settings at the start

--- lab1/histdists.m
+++ lab1/histdists.m
@@ -1,40 +1,38 @@
-function dist = histdists(Hs, method)
-% histogram distance(Hs, method)
-% every column is a histogram
+function dists = histdists(Hs, Hss, method)
+% HISTDISTS(HS, HSS, method) calculates the histogram distance between a 
+% histogram HS and a series of histograms Hss according to method
+% 
+% DISTS = HISTDISTS(HS, HSS, METHOD)
+% HS is a histogram represented as an column vector
+% HSS is a series of histograms, every column is a histogram
+% 
+% METHOD: a string
 % BC = 1 - Bhattacharyya distance
 % EU = Euclidean distance, normalised to 1
 % HI = 1- Histogram intersection
 
-[M, numH] = size(Hs);
+[nbins, nHss] = size(Hss);
+
 %normalise to be sure
-Hs = Hs./repmat(sum(Hs), M, 1);
+Hs = Hs./repmat(sum(Hs), nbins, 1);
+Hss = Hss./repmat(sum(Hss), nbins, 1);
+
+% always used
+Hs = repmat(Hs, 1, nHss);
 
 switch lower(method)
     case {'bc', 'bhattacharyya'}
         %disp('Bhattacharyya');
-        aa = repmat(Hs, numH, 1);
-        bb = reshape(Hs, M*numH, 1);
-        bb = repmat(bb, 1, numH);
-        dist = sqrt(aa.*bb);
-        dist = reshape(dist, M, numH.^2);
-        dist = 1 - reshape(sum(dist, 1), numH, numH);
+        BC = sum(sqrt(Hs.*Hss));
+        dists = sqrt(1-BC);
     case {'eu', 'euclidean'}
         %disp('Euclidean');
-        %(a-b)^2 = a^2+b^2-2ab
-        aa = sum(Hs.*Hs);  
-        ab = Hs'*Hs; 
-        aa = repmat(aa, [size(aa,2), 1]);
-        dist = sqrt( aa + aa' - 2*ab)/sqrt(2);
+        dists = sqrt(sum((Hs-Hss).^2));
     case {'hi', 'intersection'}
         % the normalisation in denominator with min(num(H1), num(H2)) not
         % needed if histogram adds up to 1 I think
         %disp('Histogram intersection');
-        aa = repmat(Hs, numH, 1);
-        bb = reshape(Hs, M*numH, 1);
-        bb = repmat(bb, 1, numH);
-        dist = reshape(min(aa, bb), M, numH.^2);
-        dist = 1 - reshape(sum(dist, 1), numH, numH);
+        dists = sum(min(Hs, Hss));
     otherwise
         disp('Unknown method');
 end
-dist = full(dist);
--- lab1/run.m
+++ lab1/run.m
@@ -2,12 +2,21 @@ close all
 clear all
 
 %% Variables
+% settings controling process flow
+COLOR = struct('RGB', 0, 'XY', 1);
+GETNEMO = struct('ON', 0, 'OFF', 1);
+PROF = struct('ON', 0, 'OFF', 1);
+settings = struct('color', COLOR.XY, 'getNemo', GETNEMO.OFF, 'prof', PROF.OFF);
 
+% other settings
 image_files = str2mat('nemo1.jpg', 'nemo2.jpg');
 image_dir = 'images';
-NBins = [20, 20, 20];
+NBins = [16,16];
 
-
+%do profiling if needed
+if settings.prof == PROF.ON
+    profile on
+end
 
 %% Read images
 
@@ -19,72 +28,113 @@ end
 
 img1 = cell2mat(imgs(1,1));
 img2 = cell2mat(imgs(1,2));
-% goto xy space
-%I_xyl1 = rgb2xy(img1);
-%I_xyl2 = rgb2xy(img2);
 
-%show original
+%% change colorspace
+if settings.color == COLOR.XY
+    % goto xy space
+    I_xyl1 = rgb2xy(img1);
+    I_xyl2 = rgb2xy(img2);
+    img = I_xyl1;
+else
+    img = img1;
+end
+%% show original
 h = figure;
 subplot(2,2,1);
 imshow(img1);
-img = img1;
-%img = I_xyl1;
-
-%get region
-rect = getrect(h)
-x = floor(rect(1));
-y = floor(rect(2));
-w = floor(rect(3));
-h = floor(rect(4));
-
 % show xy image converted back
+if settings.color == COLOR.XY
+
+    imgS = xy2rgb(img);
+else
+    imgS = img;
+end
+
 subplot(2,2,2);
-imshow(img);
-       
-% x = 249;
-% y = 104;
-% w = 62;
-% h = 71;
+
+imshow(imgS)
+
+%% get region & show
+if settings.getNemo == GETNEMO.ON
+    rect = getrect(h);
+    x = floor(rect(1));
+    y = floor(rect(2));
+    w = floor(rect(3));
+    h = floor(rect(4));
+else
+    x = 249;
+    y = 104;
+    w = 62;
+    h = 71;
+end
 
 nemo = img(y:y+h, x:x+w, :);
-%figure
-%imshow(img1(y:y+h, x:x+w, :))
-
 [M, N, P] = size(nemo);
 nemoReshaped = reshape(nemo, [M*N, P]);
 
-
-
-% get locks
+%% get nemo histogram
 %locs = img2histloc2D(nemoReshaped, NBins);
 locs = img2histloc(nemoReshaped, NBins);
+
+hist = locs2hists(locs, NBins);
 % get counts
-counts = full(sparse(locs, ones(M*N,1), ones(M*N,1), prod(NBins), 1));
-p = counts/(M*N);
+%counts = full(sparse(locs, ones(M*N,1), ones(M*N,1), prod(NBins), 1));
+%p = counts/(M*N);
 
-
+%% hist back projection
 
 % save prob on corresponding index
-%img = I_xyl2;
+%img = I_xyl2; 
 
 %img = img2;
 [M, N, P] = size(img);
+[MM, NN, PP] = size(img);
 imgReshaped = reshape(img, [M*N, P]);
 
 %imgloc = img2histloc2D(imgReshaped, NBins);
 imgloc = img2histloc(imgReshaped, NBins);
 
-result = p(imgloc);
+result = hist(imgloc);
 result = reshape(result, [M,N]);
-%%
+
+%% get sliding window histograms
+[M, N, P] = size(nemo);
+imgloc = reshape(imgloc,[MM,NN]);
+
+% calculate distances
+% this may be optimized by keeping partial counts
+dists1 = myImageFilter(imgloc, [M,N], hist, NBins, 'BC');
+dists2 = myImageFilter(imgloc, [M,N], hist, NBins, 'EU');
+dists3 = myImageFilter(imgloc, [M,N], hist, NBins, 'HI');
+
+%% plot
 subplot(2,2,3)
 imshow(img2)
 subplot(2,2,4)
-%imgT = xy2rgb(img2)
-imshow(img2)
+
+if settings.color == COLOR.XY
+    imgT = xy2rgb(img2);
+else
+    imgT = img2;
+end
+imshow(imgT)
+
 figure;
 imshow(result,[]);
 
 %show nemo
 figure
 imshow(img1(y:y+h, x:x+w, :))
+
+% show dists
+figure
+subplot(1,3,1)
+imshow(dists1)
+subplot(1,3,2)
+imshow(dists2)
+subplot(1,3,3)
+imshow(dists3)
+%show profiler
+if settings.prof == PROF.ON
+    profile viewer
+end
