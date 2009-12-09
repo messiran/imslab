9dc981f821f0c99654bc1d1b32d10948e1ca14a7
mailtjerk <mailtjerk@87a579e4-a2c4-11de-a590-a9c80ee327d0>
2009-12-09 16:55:43 +0000

    meanshift works, now optimizing

--- trunk/lab5/getSettings.m
+++ trunk/lab5/getSettings.m
@@ -15,18 +15,20 @@ function settings = readSettings()
     settings.searchNbh = [20,20];
     settings.cache = settings.CACHE.ON;
     settings.defaultRoi = [575, 230, 10, 40];
-    settings.frameRange = 85:89;
+    settings.frameRange = 85:139;
 
     % other settings
     if settings.color == settings.COLOR.XY
         settings.NBins = [16,16];
     else
-        %settings.NBins = [16,16,16];
-        settings.NBins = [32,32,32];
+        settings.NBins = [16,16,16];
+        %settings.NBins = [2,2,2];
     end
 
     % do profiling if needed
     if settings.prof == settings.PROF.ON
         profile on
+	else
+		profile off
     end
 end
--- trunk/lab5/meanShift.m
+++ trunk/lab5/meanShift.m
@@ -10,17 +10,22 @@ frames = frameReader('voetbal', settings);
 imgT = transformColor(frames(:,:,:,1), settings);
 
 %% get region of interest
-if settings.getRoi == settings.GETROI.ON
-    handle = figure;
+if (settings.getRoi == settings.GETROI.ON && (exist('Roi.mat')~=2))
+    handle = figure('Visible','off');
     imshow(imgT);
-    rect = getrect(handle)
+    rect = getrect(handle);
     Roi =  [floor(rect(1)),...
             floor(rect(2)),...
             floor(rect(3)),...
-            floor(rect(4))]
+            floor(rect(4))];
+	save('Roi.mat', 'Roi');
     close(handle);
-else
-    Roi = settings.defaultRoi;
+else 
+	if (exist('Roi.mat')==2)
+		load('Roi.mat');
+	else
+		Roi = settings.defaultRoi;
+	end
 end
 x = Roi(1); y = Roi(2); w = Roi(3); h = Roi(4);
 
@@ -34,55 +39,77 @@ vectKernel = reshape(kernel, [1, (w+1)*(h+1)]);
 
 xNewVect = [];
 % meanShift loop
-for i = 1:size(frames, 4)
+for i = 2:size(frames, 4)
+	i
     imgC = frames(:,:,:,i);
-    % get candidate image
-    [vectCLoc, vectCHist] = getHist(vectKernel, imgC, Roi, settings);
-    
-    % candidate model
-    Pu = vectCHist
 
-    % targetmodel
-    Qu = vectTHist
 
-    % define absolute start location 
-    Y0 = [x+.5*w, y + .5*h]
+	Yshift = 1;
+	% perform shift till shift is negligible 
+	while(max(abs(Yshift))>=0.5)
+		% get candidate image
+		[vectCLoc, vectCHist] = getHist(vectKernel, imgC, Roi, settings);
+		
+		% candidate model
+		Pu = vectCHist;
 
-    % the W's per bin
-    Wbin = sqrt(Qu./Pu);
-    
-    % the W's per bin per pixel in the image
-    W = Wbin(vectCLoc)
-    X = getMask(2, Roi, 'location');
-    % duplicate W 2 times in the width dim
+		% targetmodel
+		Qu = vectTHist;
 
-    Yshift = sum((W * ones(1,2)).*X) / sum(W)
-    xNew = x+Yshift(1)*.5*(w+1)
-    xNewVect = [xNewVect;xNew];
-    yNew = y+Yshift(2)*.5*(h+1)
-    close;
-    imshow(frames(:,:,:,i));
-    hold on;
-    rectangle('Position',[xNew,yNew, w, h]);
+		% define absolute start location 
+		% Y0 = [x+.5*w, y + .5*h]
+
+		% the W's per bin
+		Wbin = sqrt(Qu./Pu);
+		% solve Nan problem
+		Wbin(Pu==0) = 0;
+		% set the W's in corresponding pixel in the image
+		W = Wbin(vectCLoc);
+
+		% location filter (e.g. -2 -1 0 1 2)
+		X = getMask(2, Roi, 'location');
+
+		% duplicate W 2 times in the width dim
+		Yshift = sum((W * ones(1,2)).*X) / sum(W);
+		xNew = round(Roi(1)+Yshift(1));
+		xNewVect = [xNewVect;xNew];
+		yNew = round(Roi(2)+Yshift(2));
+		close;
+		%imshow(frames(:,:,:,i));
+		hold on;
+		%rectangle('Position',[xNew,yNew, w, h]);
+		f = frames(:,:,:,i);
+		x = Roi(1); y = Roi(2);
+        f(y,     x:x+w, :)=0;
+        f(y+h,   x:x+w, :)=0;
+        f(y:y+h, x    , :)=0;
+        f(y:y+h, x+w  , :)=0;
+
+		%rectMask = getMask(size(f),Roi,'border');
+		% TODO optimize with ones
+		%rectMask = repmat(rectMask, [1,1,3]);
+		%f(rectMask) = 0;
+		frames(:,:,:,i) = f;
+
+		% define new Roi
+		Roi(1) = xNew;
+		Roi(2) = yNew;
+	end
+
+	%framesTracked(:,:,:,i-1) = f.cdata;
+	disp('shift!');
 end
 
-xNewVect
+%movie(immovie(frames))
+saveMovie(frames, 'result.avi', 10, 100,'Cinepak');
 
-% %show profiler
-% if settings.prof == settings.PROF.ON
-%     profile viewer
-% end
-% 
-%     
-% figure
-% movie(immovie(frames))
-% saveMovie(frames, 'result.avi', 10, 100,'Cinepak');
+% show the profiler result
+if settings.prof == settings.PROF.ON
+	profile viewer 
+end
 
-
-
-
-
-
+% play movie
+!mplayer -fps 2 result.avi
 
 % todo herhistogrammen (bij min distance opnieuw histogram berekenen???
 % (regiondimensions aanpassen??))
