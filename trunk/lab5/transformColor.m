7adaed4b19a732a4ca470df3250f4e7d3c62ba66
bjbuter <bjbuter@87a579e4-a2c4-11de-a590-a9c80ee327d0>
2009-12-21 15:14:50 +0000

    color transforms

--- trunk/lab5/transformColor.m
+++ trunk/lab5/transformColor.m
@@ -1,16 +1,40 @@
 function img = transformColor(img, settings)
 
 %% change colorspace
-if settings.color == settings.COLOR.XY
-	% prepare transforms
-	C1 = makecform('srgb2xyz');
-	C2 = makecform('xyz2xyl');
+switch lower(settings.color)
+    %case {settings.COLOR.RGB}
+    
+    case {settings.COLOR.rg}   
+        r = ones(size(img(:,:,1))) * 1/3;
+        g = r;
+        
+        norm = sum(img,3);
+        mask = norm > 0;
+        r(mask) = img(:,:,1)./norm;
+        g(mask) = img(:,:,2)./norm;
+        img(:,:,1) = r;
+        img(:,:,2) = g;
+        
+    case {settings.COLOR.XY}
+        % prepare transforms
+        C1 = makecform('srgb2xyz');
+        C2 = makecform('xyz2xyl');
 
-	% transform
-	img = applycform(img,C1);
-	img = applycform(img,C2);
+        % transform
+        img = applycform(img,C1);
+        img = applycform(img,C2);
 
-	%remove luminance
-	img = img(:,:,1:2);
+        %remove luminance
+        img = img(:,:,1:2);
+    case {settings.COLOR.H}
+        img = rgb2hsv(img);
+        img = img(:,:,1);
+        
+    case {settings.COLOR.HS}
+        img = rgb2hsv(img);
+        img = img(:,:,1:2);
+    
+    case {settings.COLOR.HSV}
+        img = rgb2hsv(img);
 end
 
