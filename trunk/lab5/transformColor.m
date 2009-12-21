9d8d423a56b8a82e67efc8336fdbfbee3bd7f426
mailtjerk <mailtjerk@87a579e4-a2c4-11de-a590-a9c80ee327d0>
2009-12-21 14:29:12 +0000

    colormodels for bart

--- trunk/lab5/transformColor.m
+++ trunk/lab5/transformColor.m
@@ -1,7 +1,16 @@
 function img = transformColor(img, settings)
+
 %% change colorspace
 if settings.color == settings.COLOR.XY
-    % goto xy space
-    img = rgb2xy(imgOrigIn);
+	% prepare transforms
+	C1 = makecform('srgb2xyz');
+	C2 = makecform('xyz2xyl');
+
+	% transform
+	img = applycform(img,C1);
+	img = applycform(img,C2);
+
+	%remove luminance
+	img = img(:,:,1:2);
 end
 
