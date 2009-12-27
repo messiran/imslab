f90e8dfbe67e6e88b9ca3e887e771c51290f6da0
bjbuter <bjbuter@87a579e4-a2c4-11de-a590-a9c80ee327d0>
2009-12-27 14:45:11 +0000

    code cleanup to get into it today

--- trunk/lab5/getHist.m
+++ trunk/lab5/getHist.m
@@ -1,11 +1,45 @@
-function [vectLoc, vectHist] = getHist(vectKernel, img, Roi, settings) 
-    x = Roi(1); y = Roi(2); w = Roi(3); h = Roi(4);
-    % reshape the cropped region of interest to a standing vector
-    vect = reshape(img(y:y+h, x:x+w, :), [(h+1)*(w+1), size(img,3)]);
+function [vectLoc, vectHist] = getHist(img, Roi, settings) 
+%GETHIST get a weighted histogram of roi in img using settings and a
+%indexed version of the roi in img, with the bin numbers in the histogram
+% [VECTLOC, VECTHIST] = GETHIST(IMG, ROI, SETTINGS)
+% VECTLOC = an indexed image reshaped to a column vector, of the ROI in IMG
+%   where the index indicates the bin in the histogram 
+% VECTHIST = a weighted histogram of ROI in IMG using SETTINGS
+% IMG = an image.
+% ROI = region of interest in image as [x, y, width, height]
+% SETTINGS = settings structure (see getSettings.m)  
 
-    % construct histogram
-    % get bin location
-    vectLoc = img2histloc(vect, settings.NBins);
+% setup persistent variables as to not recalculate the kernel
+persistent wP hP vectKernelP
 
-    % count buckets
-    vectHist = locs2hists(vectLoc, settings.NBins, vectKernel);
+%unravel ROI variables
+x = Roi(1); y = Roi(2); w = Roi(3); h = Roi(4);
+
+% get weighting kernel for histogram counts, persistent if possible.
+if(isempty(wP) || (w~=wP) || (h~=hP))
+    wP = w;
+    hP = h;
+    vectKernelP = getMask(Roi, 'Epanechnikov');
+end
+vectKernel = vectKernelP;
+
+% reshape the cropped region of interest to a standing vector
+vectImg = reshape(img(y:y+h, x:x+w, :), [(h+1)*(w+1), size(img,3)]);
+
+%% construct histogram
+% get bin location
+vectLoc = img2histloc(vectImg, settings.NBins);
+
+% count buckets
+vectHist = locs2hist(vectLoc, settings.NBins, vectKernel);
+
+function hist = locs2hist(locs, NBins, colKernel)
+%LOCS2HIST returns a weighted histogram represented as a column vector
+% LOCS2HIST(LOCS, NBINS, COLKERNEL) 
+% HIST column histogram
+% LOCS histogram locations, an indexed image vector with bin locations
+% NBINS number of bins per color dimension
+% COLKERNEL weighting kernel for histogram
+
+hist = full(sparse(locs, ones(1,length(locs)), colKernel, prod(NBins), 1));
+
